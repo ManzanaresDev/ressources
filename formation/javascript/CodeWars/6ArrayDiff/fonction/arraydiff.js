d5c9650952d14cea8a84f95a08afa4ff2787e830
marcos <marcosmanzanarescampo@gmail.com>
2025-08-06 08:14:18 +0200

    formation upload

---
+++ formation/javascript/CodeWars/6ArrayDiff/fonction/arraydiff.js
@@ -0,0 +1,11 @@
+// arraydiff.js
+
+function elemInArray(elem, array) {
+  return array.includes(elem);
+}
+
+export function arrayDiff(a, b) {
+  if(Array.isArray(a))
+  const union = new setInterval([...a], [...b]);
+  return union;
+}
